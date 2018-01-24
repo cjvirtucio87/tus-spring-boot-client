58ff7fe8ef528e225fdc691b6c53cb52e7674551
cjvirtucio87 <cjv287@gmail.com>
2018-01-23 20:47:35 -0500

    chore(Git): ignore
    - adjusted gitignore

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import container from './container';
+
+const { Dashboard } = container;
+
+const App = () => (
+  <div className="App">
+    <Dashboard/>
+  </div>
+);
+
+export default App;
