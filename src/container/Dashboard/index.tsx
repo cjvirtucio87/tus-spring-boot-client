07218cedf02fd7b7bb8ee8ce875686d10b003416
cvirtucio <cvirtucio@mitre.org>
2018-03-20 18:54:19 -0400

    content-type refers to acceptable response

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -178,9 +178,9 @@ const DownloadBtn = ({ fileName }) => {
   const onClick = () => {
       axios.get(`localhost:8080/download/file`, {
           headers: {
-              fileName
-          },
-          responseType: 'application/octet-stream' 
+              fileName,
+              'content-type': 'application/octet-stream'
+          }
       })
       .then(data => {
           console.log(data);
