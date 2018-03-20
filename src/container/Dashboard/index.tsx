cfa693753022f47a0a7ecbec7550cb18c0bd7b04
cvirtucio <cvirtucio@mitre.org>
2018-03-20 19:14:46 -0400

    content-type in header

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -178,9 +178,9 @@ const DownloadBtn = ({ fileName }) => {
   const onClick = () => {
       axios.get(`localhost:8080/download/file`, {
           headers: {
-              fileName          
-          },
-          contentType: 'application/octet-stream'
+              fileName,
+              'Content-Type': 'application/octet-stream'
+          }, 
       })
       .then(data => {
           console.log(data);
