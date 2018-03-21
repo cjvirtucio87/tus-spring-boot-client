983a7fc58c819ba53bf7f9d569e3749b73784f4e
cvirtucio <cvirtucio@mitre.org>
2018-03-21 08:15:56 -0400

    importing whole FileSaver object

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 import { PART_SIZE, BASE_URI, FILENAME_PATTERN } from '../../constants';
 import { addFile, updateProgress, finishUpload, toggleChunkMode, showFilename } from '../../actions';
 
-import { saveAs } from 'file-saver';
+import * as FileSaver from 'file-saver';
 
 import presentational from '../../presentational/';
 
@@ -188,7 +188,7 @@ const DownloadBtn = ({ fileName }) => {
       .then(res => {
           const json = JSON.stringify(res.data);
           const blob = new Blob([json], { type: 'octet/stream' });
-          saveAs(blob, fileName);
+          FileSaver.saveAs(blob, fileName);
       })
       .catch(err => {
           if (err.response) {
