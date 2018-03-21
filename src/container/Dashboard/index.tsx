cc0a19376f0ee5fbb72032bccdb8a5130993026e
cvirtucio <cvirtucio@mitre.org>
2018-03-21 07:26:04 -0400

    changed unmarshalling

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -184,12 +184,13 @@ const DownloadBtn = ({ fileName }) => {
           responseType: 'stream'
       })
       .then(res => {
-          const url = window.URL.createObjectURL( new Blob([res.data]) );
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', 'file.pdf');
-          document.body.appendChild(link);
-          link.click();
+          const json = JSON.stringify(res.data);
+          const blob = new Blob([json], { type: 'octet/stream' });
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.download = fileName;
+          a.click();
+          window.URL.revokeObjectURL(url);
       })
       .catch(err => {
           if (err.response) {
