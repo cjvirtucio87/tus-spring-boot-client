a701fd0ad3bc8cece02283ceb93d551605823f36
Christopher Virtucio <cjvirtucio@CJs-MacBook-Pro.local>
2018-12-16 11:55:05 -0500

    added types to actions

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,20 +1,24 @@
+import { FilePart } from "../data/file-part";
+import { ProgressParams } from "../data/progress-params";
+import { FileMetadata } from "../data/file-metadata";
+
 /* 
   Actions are messages containing with payload for updating state.
 
   Note the syntax regarding the payload: { file } and { progressParams } are synctatic sugar for { file: file } and { progressParams: progressParams } (ES6 feature).
 */
 
-export const addFile = (parts) => ({
+export const addFile = (parts: Array<FilePart>) => ({
   type: 'ADD_FILE',
   parts
 });
 
-export const uploadPart = (part) => ({
+export const uploadPart = (part: FilePart) => ({
   type: 'UPLOAD_PART',
   part
 });
 
-export const updateProgress = (progressParams) => ({
+export const updateProgress = (progressParams: ProgressParams) => ({
   type: 'UPDATE_PROGRESS',
   progressParams
 });
@@ -27,7 +31,7 @@ export const toggleChunkMode = () => ({
   type: 'TOGGLE_CHUNK_MODE'
 });
 
-export const setFileMetadata = (fileMetadata) => ({
+export const setFileMetadata = (fileMetadata: FileMetadata) => ({
   type: 'SHOW_FILEMETADATA',
   fileMetadata
 });
